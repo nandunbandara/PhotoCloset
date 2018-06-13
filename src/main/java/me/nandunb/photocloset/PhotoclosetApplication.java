e0e55cb6eb2c61eb6466d411f0ab00a174ad7913
Nandun Bandara <ntbandara3@hotmail.com>
2018-06-13 21:52:57 +0530

    init

---
+++ src/main/java/me/nandunb/photocloset/PhotoclosetApplication.java
@@ -0,0 +1,12 @@
+package me.nandunb.photocloset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoclosetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoclosetApplication.class, args);
+	}
+}
---
+++ src/test/java/me/nandunb/photocloset/PhotoclosetApplicationTests.java
@@ -0,0 +1,16 @@
+package me.nandunb.photocloset;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoclosetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
