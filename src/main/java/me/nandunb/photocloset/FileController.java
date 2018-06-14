187a9644b06c97a37df3bd43b41e901357986bff
Nandun Bandara <ntbandara3@hotmail.com>
2018-06-14 09:44:41 +0530

    Frontend changes

--- src/main/java/me/nandunb/photocloset/FileController.java
+++ src/main/java/me/nandunb/photocloset/FileController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/storage/")
+@RequestMapping("/storage")
 public class FileController {
 
     private AmazonClient amazonClient;
